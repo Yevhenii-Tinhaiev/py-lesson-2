adbe8604ef964ed32d57cfae61e185cd2fe29132
Yevhenii <e.tingaev@gmail.com>
2025-03-04 23:21:25 +0200

    lesson_3.3

--- lesson_3.3.py
+++ lesson_3.3.py
@@ -1,8 +1,8 @@
-nums = []
-middle_index = len(nums) // 2
-if len(nums) % 2 != 0:
+numbers = [2, 5, 7, 8]
+middle_index = len(numbers) // 2
+if len(numbers) % 2 != 0:
     middle_index += 1
-part1 = nums[:middle_index]
-part2 = nums[middle_index:]
+part1 = numbers[:middle_index]
+part2 = numbers[middle_index:]
 result = [part1, part2]
 print(result)
