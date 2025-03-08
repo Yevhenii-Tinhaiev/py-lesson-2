865e79725c711085acf46b13cd23a28969cfab66
Yevhenii <e.tingaev@gmail.com>
2025-03-08 13:05:53 +0200

    lesson_4.1

---
+++ lesson_4.1.py
@@ -0,0 +1,11 @@
+numbers = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
+
+i = 0
+for item in numbers:
+    if item != 0:
+        numbers[i] = item
+        i += 1
+while i < len(numbers):
+    numbers[i] = 0
+    i += 1
+print(numbers)
